82d1578000761aab24d934a200511a525102a252
towhid7667 <towhidulhq@gmail.com>
2024-03-15 00:24:18 +0600

    OOP demonstation started

---
+++ create_class_objects.py
@@ -0,0 +1,12 @@
+class Employee:
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+
+e1 = Employee("Vera", 2000)
+e2 = Employee("Chuck", 1800)
+e3 = Employee("Dave", 1900)
+
+print(e1)
+print(e2)
+print(e3)
